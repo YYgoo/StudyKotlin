701f5d3e351c54af7edba3150c5c62d3d80da36d
张耀 <443231186@qq.com>
2018-10-30 09:56:07 +0800

    zyy test

--- app/src/main/java/com/example/a44323/studykotlin/ui/activity/GitTest.java
+++ app/src/main/java/com/example/a44323/studykotlin/ui/activity/GitTest.java
@@ -4,9 +4,9 @@ package com.example.a44323.studykotlin.ui.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 
-public class Test extends AppCompatActivity {
+public class GitTest extends AppCompatActivity {
 
         private RecyclerView mRecyclerView = null;
         byte[] b = new byte[100];
         String msg ="sss";
-        }
+}
